57d69fae939244022df982496c6f846757775d6a
Parham Najafi Fakher <73719746+parawm@users.noreply.github.com>
2023-12-01 20:42:48 +0330

    State/Strategy Pattern method extraction

---
+++ src/main/java/parser/AcceptActionStrategy.java
@@ -0,0 +1,9 @@
+package parser;
+import scanner.token.Token;
+
+public class AcceptActionStrategy implements ActionStrategy {
+    @Override
+    public void executeAction(Action currentAction, Token lookAhead) {
+
+    }
+}
--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -46,67 +46,83 @@ public class Parser {
 //        }
 //        cg = new CodeGenerator();
 //    }
-
-    public void startParse(java.util.Scanner sc) {
-        lexicalAnalyzer = new lexicalAnalyzer(sc);
-        Token lookAhead = lexicalAnalyzer.getNextToken();
-        boolean finish = false;
-        Action currentAction;
-        while (!finish) {
-            try {
-                Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
-//                Log.print("state : "+ parsStack.peek());
-                currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
-                Log.print(currentAction.toString());
-                //Log.print("");
-
-                switch (currentAction.action) {
-                    case shift:
-                        parsStack.push(currentAction.number);
-                        lookAhead = lexicalAnalyzer.getNextToken();
-
-                        break;
-                    case reduce:
-                        Rule rule = rules.get(currentAction.number);
-                        for (int i = 0; i < rule.RHS.size(); i++) {
-                            parsStack.pop();
-                        }
-
-                        Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.LHS);
-//                        Log.print("LHS : "+rule.LHS);
-                        parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.LHS));
-                        Log.print(/*"new State : " + */parsStack.peek() + "");
-//                        Log.print("");
-                        try {
-                            cg.semanticFunction(rule.semanticAction, lookAhead);
-                        } catch (Exception e) {
-                            Log.print("Code Genetator Error");
-                        }
-                        break;
-                    case accept:
-                        finish = true;
-                        break;
-                }
-                Log.print("");
-            } catch (Exception ignored) {
-                ignored.printStackTrace();
-//                boolean find = false;
-//                for (NonTerminal t : NonTerminal.values()) {
-//                    if (parseTable.getGotoTable(parsStack.peek(), t) != -1) {
-//                        find = true;
-//                        parsStack.push(parseTable.getGotoTable(parsStack.peek(), t));
-//                        StringBuilder tokenFollow = new StringBuilder();
-//                        tokenFollow.append(String.format("|(?<%s>%s)", t.name(), t.pattern));
-//                        Matcher matcher = Pattern.compile(tokenFollow.substring(1)).matcher(lookAhead.toString());
-//                        while (!matcher.find()) {
-//                            lookAhead = lexicalAnalyzer.getNextToken();
-//                        }
-//                    }
-//                }
-//                if (!find)
-//                    parsStack.pop();
-            }
+public void startParse(java.util.Scanner sc) {
+    lexicalAnalyzer = new lexicalAnalyzer(sc);
+    Token lookAhead = lexicalAnalyzer.getNextToken();
+    boolean finish = false;
+    Action currentAction;
+    while (!finish) {
+        try {
+            currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
+            ActionStrategy strategy = ActionStrategyFactory.getStrategy(currentAction.action);
+            strategy.executeAction(currentAction, lookAhead);
+        } catch (Exception ignored) {
+            ignored.printStackTrace();
         }
-        if (!ErrorHandler.hasError) cg.printMemory();
     }
+    if (!ErrorHandler.hasError) cg.printMemory();
+}
+
+//    public void startParse(java.util.Scanner sc) {
+//        lexicalAnalyzer = new lexicalAnalyzer(sc);
+//        Token lookAhead = lexicalAnalyzer.getNextToken();
+//        boolean finish = false;
+//        Action currentAction;
+//        while (!finish) {
+//            try {
+//                Log.print(/*"lookahead : "+*/ lookAhead.toString() + "\t" + parsStack.peek());
+////                Log.print("state : "+ parsStack.peek());
+//                currentAction = parseTable.getActionTable(parsStack.peek(), lookAhead);
+//                Log.print(currentAction.toString());
+//                //Log.print("");
+//
+//                switch (currentAction.action) {
+//                    case shift:
+//                        parsStack.push(currentAction.number);
+//                        lookAhead = lexicalAnalyzer.getNextToken();
+//
+//                        break;
+//                    case reduce:
+//                        Rule rule = rules.get(currentAction.number);
+//                        for (int i = 0; i < rule.RHS.size(); i++) {
+//                            parsStack.pop();
+//                        }
+//
+//                        Log.print(/*"state : " +*/ parsStack.peek() + "\t" + rule.LHS);
+////                        Log.print("LHS : "+rule.LHS);
+//                        parsStack.push(parseTable.getGotoTable(parsStack.peek(), rule.LHS));
+//                        Log.print(/*"new State : " + */parsStack.peek() + "");
+////                        Log.print("");
+//                        try {
+//                            cg.semanticFunction(rule.semanticAction, lookAhead);
+//                        } catch (Exception e) {
+//                            Log.print("Code Genetator Error");
+//                        }
+//                        break;
+//                    case accept:
+//                        finish = true;
+//                        break;
+//                }
+//                Log.print("");
+//            } catch (Exception ignored) {
+//                ignored.printStackTrace();
+////                boolean find = false;
+////                for (NonTerminal t : NonTerminal.values()) {
+////                    if (parseTable.getGotoTable(parsStack.peek(), t) != -1) {
+////                        find = true;
+////                        parsStack.push(parseTable.getGotoTable(parsStack.peek(), t));
+////                        StringBuilder tokenFollow = new StringBuilder();
+////                        tokenFollow.append(String.format("|(?<%s>%s)", t.name(), t.pattern));
+////                        Matcher matcher = Pattern.compile(tokenFollow.substring(1)).matcher(lookAhead.toString());
+////                        while (!matcher.find()) {
+////                            lookAhead = lexicalAnalyzer.getNextToken();
+////                        }
+////                    }
+////                }
+////                if (!find)
+////                    parsStack.pop();
+//            }
+//        }
+//        if (!ErrorHandler.hasError) cg.printMemory();
+//    }
 }
